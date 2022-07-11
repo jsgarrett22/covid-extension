5d5785f98a51e1838a5d285254b1940fcadfad06
jsgarrett22 <jgarrett35@student.cscc.edu>
2022-07-11 18:48:26 -0400

    Changed API to Covid Act Now API

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,19 @@
+// api key 2a6b425b45eb4e788c7a9d60eac08669
+
+let key = config.US_API_KEY;
+let url = "https://api.covidactnow.org/v2/country/US.json?apiKey=" + key;
+
 async function fetchData() {
-    const res = await fetch("https://api.coronavirus.data.gov.uk/v1/data");
-    const record = await res.json();
-    console.log(record);
-    document.getElementById("date").innerHTML = record.data[0].date;
-    document.getElementById("areaName").innerHTML = record.data[0].areaName;
-    document.getElementById("latestBy").innerHTML = record.data[0].confirmed;
-    document.getElementById("deathNew").innerHTML = record.data[0].deathNew;
+    const res = await fetch(url);
+    if (res.status != 200) {
+        console.log("An error occurred with the response.");
+    } else {
+        const record = await res.json();
+        document.querySelector('#date').innerHTML = record.lastUpdatedDate;
+        document.querySelector('#state').innerHTML = record.country;
+        document.querySelector('#newCases').innerHTML = record.actuals.newCases;
+        document.querySelector('#newDeaths').innerHTML = record.actuals.newDeaths;
+    }
 }
 
 fetchData();
